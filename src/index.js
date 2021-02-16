a90cb17353583f5c879f2b51ce7fe87ad04b698b
Aleksandr Zalutskiy <simplesash@gmail.com>
2021-02-16 13:06:32 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (array) {
+    return Math.min(...array);
+  } else {
+    return 0;
+  }
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  if (array) {
+    return Math.max(...array);
+  } else {
+    return 0;
+  }
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (array) {
+    return array.reduce((a, b) => (a + b)) / array.length;
+  } else {
+    return 0;
+  }
 }
